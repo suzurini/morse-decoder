f1ffa01065c05826c1056a56b337ccf15ed6f950
suzurini <valloforr@gmail.com>
2023-08-22 19:02:02 +0300

    fix: fixed decode for sentenses

--- src/index.js
+++ src/index.js
@@ -39,6 +39,45 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let word ='';
+    let k = 0;
+    let countChar = expr.length / 10;
+    const sepExpr = [];
+    while (k < countChar) {
+        if (expr.length) {
+            sepExpr.push(expr.slice(0, 10));
+            expr = expr.slice(10);
+        }
+        k++;
+    }
+    for (let exprItem of sepExpr) {
+        let i = 0;
+        let strChar = '';
+        if (exprItem.indexOf('*') !== -1) {
+            exprItem = [];
+            word = word + ' ';
+        }
+        else {
+            while (i < 10) {
+                let charNumber1 = exprItem.indexOf('1');
+                i = i + charNumber1 + 2;
+                if (exprItem[charNumber1 + 1] == '1') {
+                    strChar = strChar + '-';
+                }
+                else {
+                    strChar = strChar + '.';
+                }
+                if (i <= 8) {
+                    exprItem = exprItem.slice(charNumber1 + 2);
+                }
+                else {
+                    exprItem = [];
+                }
+            }
+            word = word + MORSE_TABLE[strChar];
+        }
+    }
+    return word;
 }
 
 module.exports = {
